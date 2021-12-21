ee0166ffd26625279fad232ec5f6dcabf9e73cce
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2021-12-21 11:47:10 +0000

    Update ACPCore_Cordova.m

--- src/ios/ACPCore_Cordova.m
+++ src/ios/ACPCore_Cordova.m
@@ -193,7 +193,7 @@
 - (void) setPrivacyStatus:(CDVInvokedUrlCommand*)command {
     [self.commandDelegate runInBackground:^{
         ACPMobilePrivacyStatus privacyStatus;
-        switch (ACPMobilePrivacyStatus)[self getCommandArg:command.arguments[0]] {
+        switch (int) (ACPMobilePrivacyStatus)[self getCommandArg:command.arguments[0]] {
             case 0:
                 privacyStatus = ACPMobilePrivacyStatusOptIn;
             case 1:
