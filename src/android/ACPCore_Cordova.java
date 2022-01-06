75265e8ab261eefb8f12f81e76e5ba6c71d7391c
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2022-01-06 15:10:00 +0000

    Update ACPCore_Cordova.java

--- src/android/ACPCore_Cordova.java
+++ src/android/ACPCore_Cordova.java
@@ -35,7 +35,7 @@ import com.adobe.marketing.mobile.WrapperType;
 import com.adobe.marketing.mobile.Campaign;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging;
+import com.google.firebase.installations.FirebaseInstallationsApi;
 //import com.google.firebase.iid.FirebaseInstanceId;
 //import com.google.firebase.iid.InstanceIdResult;
 import android.os.Bundle;
@@ -569,7 +569,7 @@ public class ACPCore_Cordova extends CordovaPlugin {
        final Context context = this.cordova.getActivity().getApplicationContext();
         FirebaseApp.initializeApp(context);
   
-        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(new OnCompleteListener<String>() {
             @Override 
             public void onComplete(@NonNull Task<String> task) {
                
