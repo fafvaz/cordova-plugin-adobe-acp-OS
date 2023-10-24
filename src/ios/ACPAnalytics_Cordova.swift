95ea248114bb2cf26f48c18a4c9175f276fb4013
olrvronelio <66467630@galp.com>
2023-10-24 20:20:42 -0300

    revision: ACPAnalytics_Cordova

--- src/ios/ACPAnalytics_Cordova.swift
+++ src/ios/ACPAnalytics_Cordova.swift
@@ -1,87 +1,78 @@
-/*
- Copyright 2020 Adobe. All rights reserved.
- This file is licensed to you under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License. You may obtain a copy
- of the License at http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software distributed under
- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- OF ANY KIND, either express or implied. See the License for the specific language
- governing permissions and limitations under the License.
- */
+import ACPAnalytics
 
-/********* cordova-acpanalytics.m Cordova Plugin Implementation *******/
+@objc(ACPAnalytics_Cordova) class ACPAnalytics_Cordova : CDVPlugin {
 
-//#import <Cordova/CDV.h>
-//#import <ACPAnalytics/ACPAnalytics.h>
-//#import <Cordova/CDVPluginResult.h>
-
-
-class ACPAnalytics_Cordova : CDVPlugin {
-
+    @objc(extensionVersion:)
     func extensionVersion(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+        self.commandDelegate.run(inBackground: {
             var pluginResult:CDVPluginResult! = nil
             let extensionVersion:String! = ACPAnalytics.extensionVersion()
 
-            if extensionVersion != nil && extensionVersion.length() > 0 {
-                pluginResult = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK, messageAsString:extensionVersion)
+            if extensionVersion != nil && extensionVersion.count > 0 {
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:extensionVersion)
             } else {
-                pluginResult = CDVPluginResult.resultWithStatus(CDVCommandStatus_ERROR)
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
             }
 
-            self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
         })
     }
 
+    @objc(sendQueuedHits:)
     func sendQueuedHits(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+        self.commandDelegate.run(inBackground: {
             ACPAnalytics.sendQueuedHits()
-            let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK)
-            self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK)
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
         })
     }
 
-    func clearQueue(comman:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+    @objc(clearQueue:)
+    func clearQueue(command:CDVInvokedUrlCommand!) {
+        self.commandDelegate.run(inBackground: {
             ACPAnalytics.clearQueue()
-            let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK)
-            self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK)
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
         })
     }
 
+    @objc(getQueueSize:)
     func getQueueSize(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+        self.commandDelegate.run(inBackground: {
             ACPAnalytics.getQueueSize({ (queueSize:UInt) in
-                let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK, messageAsString:String(format:"%@",  queueSize))
-                self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:String(format:"%@",  queueSize))
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
             })
         })
     }
 
+    @objc(getTrackingIdentifier:)
     func getTrackingIdentifier(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+        self.commandDelegate.run(inBackground: {
             ACPAnalytics.getTrackingIdentifier({ (trackingIdentifier:String?) in
-                let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK, messageAsString:trackingIdentifier)
-                self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:trackingIdentifier)
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
             })
         })
     }
-
+    
+    @objc(getVisitorIdentifier:)
     func getVisitorIdentifier(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
+        self.commandDelegate.run(inBackground: {
             ACPAnalytics.getVisitorIdentifier({ (visitorIdentifier:String?) in
-                let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK, messageAsString:visitorIdentifier)
-                self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK, messageAs:visitorIdentifier)
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
             })
         })
     }
 
+    @objc(setVisitorIdentifier:)
     func setVisitorIdentifier(command:CDVInvokedUrlCommand!) {
-        self.commandDelegate.runInBackground({
-            let vid:String! = command.arguments.objectAtIndex(0)
-            ACPAnalytics.visitorIdentifier = vid
-            let pluginResult:CDVPluginResult! = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK)
-            self.commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        self.commandDelegate.run(inBackground: {
+            let vid:String! = command.arguments[0] as? String
+            ACPAnalytics.setVisitorIdentifier(vid)
+            let pluginResult:CDVPluginResult! = CDVPluginResult(status: CDVCommandStatus_OK)
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
         })
     }
 }
