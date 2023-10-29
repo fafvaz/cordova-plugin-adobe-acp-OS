639f534a91ee9f4e51af7fb6458589d2d62a895c
olrvronelio <66467630@galp.com>
2023-10-29 13:13:33 -0300

    revision ACPAppDelegatePush

--- src/ios/ACPAppDelegatePush.swift
+++ src/ios/ACPAppDelegatePush.swift
@@ -1,35 +1,19 @@
-//#import "AppDelegate+Push.h"
-//#import <ACPCore/ACPCore.h>
-//#import <ACPCampaign/ACPCampaign.h>
-//#import <objc/runtime.h>
+import ACPCore
 
-extension ACPAppDelegatePush {
 
-    class func load() {
-        let original:Method = class_getInstanceMethod(self, Selector("application:didRegisterForRemoteNotificationsWithDeviceToken:"))
-        let swizzled:Method = class_getInstanceMethod(self, Selector("application:swizzleddidRegisterForRemoteNotificationsWithDeviceToken:"))
-        method_exchangeImplementations(original, swizzled)
-    }
+func application(application:UIApplication!, swizzleddidRegisterForRemoteNotificationsWithDeviceToken deviceToken:Data!) {
+  ACPCore.setPushIdentifier(deviceToken)
+  NSLog("ACPCampaign_didRegisterForRemoteNotificationsWithDeviceToken")
+}
 
-    func application(application:UIApplication!, swizzleddidRegisterForRemoteNotificationsWithDeviceToken deviceToken:NSData!) {
-      ACPCore.pushIdentifier = deviceToken
-      NSLog("ACPCampaign_didRegisterForRemoteNotificationsWithDeviceToken")
-    }
+func application(application:UIApplication!, didFailToRegisterForRemoteNotificationsWithError error:NSError!) {
+    NSLog("ACPCampaign_didFailToRegisterForRemoteNotificationsWithError")
+}
 
-    /*- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-        //[ACPCore setPushIdentifier:deviceToken];
-        NSLog(@"ACPCampaign_didRegisterForRemoteNotificationsWithDeviceToken");
-    }*/
+func application(application:UIApplication!, didReceiveRemoteNotification userInfo:NSDictionary!, fetchCompletionHandler handler:(UIBackgroundFetchResult)->Void) {
+    NSLog("ACPCampaign_didReceiveNotification")
+}
 
-    func application(application:UIApplication!, didFailToRegisterForRemoteNotificationsWithError error:NSError!) {
-        NSLog("ACPCampaign_didFailToRegisterForRemoteNotificationsWithError")
-    }
-
-    func application(application:UIApplication!, didReceiveRemoteNotification userInfo:NSDictionary!, fetchCompletionHandler handler:(UIBackgroundFetchResult)->Void) {
-        NSLog("ACPCampaign_didReceiveNotification")
-    }
-
-    func applicationDidBecomeActive(application:UIApplication!) {
-        application.applicationIconBadgeNumber = 0
-    }
+func applicationDidBecomeActive(application:UIApplication!) {
+    application.applicationIconBadgeNumber = 0
 }
