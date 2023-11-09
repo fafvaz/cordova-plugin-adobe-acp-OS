d1a0be4cae483a7d4c3ad84db3d3d6c1063f5f82
olrvronelio <66467630@galp.com>
2023-11-09 11:06:47 -0300

    fix delegate

--- src/ios/ACPAppDelegatePush.swift
+++ src/ios/ACPAppDelegatePush.swift
@@ -10,39 +10,43 @@ import AEPSignal
 import AEPTarget
 import AEPUserProfile
 
-func application(
-  _ application: UIApplication,
-  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-) -> Bool {
-  let appState = application.applicationState
-  let appId = Bundle.main.object(forInfoDictionaryKey: "AppId") as! String
+@objc class AppACPDelegate: CDVAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    let appState = application.applicationState
+    let appId = Bundle.main.object(forInfoDictionaryKey: "AppId") as! String
 
-  MobileCore.registerExtensions(
-    [
-      Signal.self,
-      Lifecycle.self,
-      UserProfile.self,
-      Identity.self,
-      Assurance.self,
-      Campaign.self,
-      Places.self,
-      Analytics.self,
-      AEPMobileServices.self,
-      Target.self,
-      Assurance.self,
-    ],
-    {
-      MobileCore.configureWith(appId: appId)
-      if appState != .background {
-        // only start lifecycle if the application is not in the background
-        MobileCore.lifecycleStart(additionalContextData: nil)
-      }
-    })
-  return true
-}
+    MobileCore.registerExtensions(
+      [
+        Signal.self,
+        Lifecycle.self,
+        UserProfile.self,
+        Identity.self,
+        Assurance.self,
+        Campaign.self,
+        Places.self,
+        Analytics.self,
+        AEPMobileServices.self,
+        Target.self,
+        Assurance.self,
+      ],
+      {
+        MobileCore.configureWith(appId: appId)
+        if appState != .background {
+          // only start lifecycle if the application is not in the background
+          MobileCore.lifecycleStart(additionalContextData: nil)
+        }
+      })
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 
-func application(
-  _ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-) {
-  MobileCore.setPushIdentifier(deviceToken)
+  override func application(
+    _ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    MobileCore.setPushIdentifier(deviceToken)
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+
 }
