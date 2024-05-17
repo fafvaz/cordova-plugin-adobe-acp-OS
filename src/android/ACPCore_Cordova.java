4c41ea7bd2bb663548026e5ac2877afbfff03ae4
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2024-05-16 22:53:20 -0300

    Update ACPCore_Cordova.java

--- src/android/ACPCore_Cordova.java
+++ src/android/ACPCore_Cordova.java
@@ -507,6 +507,7 @@ public class ACPCore_Cordova extends CordovaPlugin {
     public void onResume(boolean multitasking) {
         super.onResume(multitasking);
         MobileCore.setApplication(this.cordova.getActivity().getApplication());
+        ACPFirebaseMessagingService.handleMessage(this.cordova.getActivity().getIntent().getExtras());
         MobileCore.lifecycleStart(null);
     }
  
